ccca1818dfb978f387a4373fccd64e25e04538dc
wilner.claro <wilner.claro@mds.gov.br>
2026-05-23 09:31:22 -0300

    feat: criação da entidade, mapper e repository

---
+++ src/main/java/br/com/wilner/couponapi/mapper/CouponMapper.java
@@ -0,0 +1,32 @@
+package br.com.wilner.couponapi.mapper;
+
+import br.com.wilner.couponapi.domain.model.Coupon;
+import br.com.wilner.couponapi.persistence.entity.CouponEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface CouponMapper {
+
+    CouponEntity toEntity(Coupon coupon);
+
+    void updateEntityFromDomain(Coupon coupon, @MappingTarget CouponEntity entity);
+
+    default Coupon toDomain(CouponEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return Coupon.restore(
+                entity.getId(),
+                entity.getCode(),
+                entity.getDescription(),
+                entity.getDiscountValue(),
+                entity.getExpirationDate(),
+                entity.getStatus(),
+                entity.isPublished(),
+                entity.isRedeemed(),
+                entity.isDeleted()
+        );
+    }
+}
---
+++ src/main/java/br/com/wilner/couponapi/persistence/repository/CouponRepository.java
@@ -0,0 +1,12 @@
+package br.com.wilner.couponapi.persistence.repository;
+
+import br.com.wilner.couponapi.persistence.entity.CouponEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<CouponEntity, UUID> {
+
+    Optional<CouponEntity> findByIdAndDeletedFalse(UUID id);
+}
