fc9f340debe2f63f71f0c3673a430e3d53e152ab
wilner.claro <wilner.claro@mds.gov.br>
2026-05-23 08:27:01 -0300

    chore: primeiro commit , inicio do projeto

---
+++ src/test/java/br/com/wilner/couponapi/CouponApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wilner.couponapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
